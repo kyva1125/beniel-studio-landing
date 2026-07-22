1457dcefc40cc70ab93d551307bf372288bbe680
Nick Ledesma <48269927+kyva1125@users.noreply.github.com>
2026-07-22 12:27:46 -0500

    feat: add Vercel Web Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { StudioLanding } from './pages/StudioLanding/StudioLanding';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-  return <StudioLanding />;
+  return <><StudioLanding /><Analytics /></>;
 }
 
 export default App;
